dceade88e737c3c5396bca5d1c2b6b0b213ec9c8
Carson H Franklin <carson.franklin@outlook.com>
2023-03-09 05:05:25 +0000

    Banner

--- src/components/Pages/TeamPage/TeamBanner.js
+++ src/components/Pages/TeamPage/TeamBanner.js
@@ -7,16 +7,18 @@ const TeamBanner = () => {
         className="breadcrumb__area include-bg breadcrumb__overlay  team_banner"
         // data-background="assets/img/breadcrumb/breadcrumb-bg-2.jpg"
         style={{
-          backgroundImage: `url("assets/img/breadcrumb/garden3.png")`,
+          backgroundImage: `url("assets/img/breadcrumb/2555.png")`,
         }}
       >
         <div className="container">
           <div className="row">
             <div className="col-xxl-12">
               <div className="breadcrumb__content text-center p-relative z-index-1">
-                <h3 className="breadcrumb__title">How do you tell your story?</h3>
+                <h3 className="breadcrumb__title">
+                  How do you tell your story?
+                </h3>
                 <div className="breadcrumb__list">
-              {/*     <span>
+                  {/*     <span>
                     <a href="#">Home</a>
                   </span>
                   <span className="dvdr">:</span>
