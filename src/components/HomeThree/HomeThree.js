e518659ef94295c38de9a630a005f3d16d70618b
Carson H Franklin <carson.franklin@outlook.com>
2023-03-31 04:59:40 +0000

    Cleanup

--- src/components/HomeThree/HomeThree.js
+++ src/components/HomeThree/HomeThree.js
@@ -1,34 +1,24 @@
 import React from "react";
-
 import Menu from "../common/Menu/Menu";
 import AboutThree from "./AboutThree";
-import BlogThree from "./BlogThree";
 import OurService from "../Pages/Service/OurService";
 import ContactThree from "./ContactThree";
 import SliderHomeThree from "./SliderHomeThree";
 import FAQBanner from "../Pages/FAQ/FaqBanner";
 import Accordion from "../Pages/FAQ/Accordion";
-import TopCtaArea from "../Home/TopCtaArea";
-import TeamBanner from "../Pages/TeamPage/TeamBanner";
 import FooterTwo from "../common/Footer/FooterTwo";
-
-
 
 const HomeThree = () => {
   return (
     <>
       <Menu header_style_2={true} />
-      
       <SliderHomeThree />
       <AboutThree />
       <OurService />
-      {/* <TopCtaArea /> */}
-      
-      <FAQBanner/>
+      <FAQBanner />
       <Accordion />
-      {/* <BlogThree /> */}
       <ContactThree />
-   <FooterTwo />
+      <FooterTwo />
     </>
   );
 };
